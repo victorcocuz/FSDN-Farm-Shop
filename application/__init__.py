2de5b5974b7c1be62eab490699c4c74aa3cfed9a
Vitor Cocuz <victor.cocuz@yahoo.com>
2020-04-22 21:09:54 +0100

    testing different configurations for heroku

--- application/__init__.py
+++ application/__init__.py
@@ -1,19 +1,19 @@
 from flask import Flask, jsonify
 import logging
 
-def create_app(test_config=None):
-	app = Flask(__name__, instance_relative_config=True)
+# def create_app(test_config=None):
+app = Flask(__name__, instance_relative_config=True)
 
-	logging.basicConfig()
-	logging.getLogger().setLevel(logging.DEBUG)
+logging.basicConfig()
+logging.getLogger().setLevel(logging.DEBUG)
 
-	@app.route('/')
-	def hello():
-		return jsonify({'message': 'HELLO WOLRD'})
+@app.route('/')
+def hello():
+	return jsonify({'message': 'HELLO WOLRD'})
 
-	return app
+# 	return app
 
 # APP = create_app()
 
 # if __name__ == '__main__':
-#     APP.run(port=8080, debug=True)
+#     APP.run(host='0.0.0.0', port=8080, debug=True)
