16645738a5a069d6a36be9224dbbc50e0906f6c2
Vitor Cocuz <victor.cocuz@yahoo.com>
2020-04-22 14:55:46 +0100

    initial deploy

--- application/__init__.py
+++ application/__init__.py
@@ -4,10 +4,11 @@ import logging
 def create_app(test_config=None):
 	app = Flask(__name__)
 
+	logging.basicConfig()
+	logging.getLogger().setLevel(logging.DEBUG)
+
 	@app.route('/')
 	def hello():
-		print ("whatever")
-		logging.error('come on')
 		return jsonify({'message': 'HELLO WOLRD'})
 
 	return app
