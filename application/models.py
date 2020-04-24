064a79e8df6113ecf539fe10a83a1a3a8c08a67b
Vitor Cocuz <victor.cocuz@yahoo.com>
2020-04-24 22:36:24 +0100

    reverted to original psql address

--- application/models.py
+++ application/models.py
@@ -4,12 +4,12 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 def get_database_path(app):
-	# database_name = app.config.get('DATABASE_NAME')
-	# database_user = app.config.get('DATABASE_USER')
-	# database_password = app.config.get('DATABASE_PASSWORD')
-	# database_host = app.config.get('DATABASE_HOST')
-	# return "postgres://{}:{}@{}/{}".format(database_user, database_password, database_host, database_name)
-	return "postgresql:///fsdn-farm-shop"
+	database_name = app.config.get('DATABASE_NAME')
+	database_user = app.config.get('DATABASE_USER')
+	database_password = app.config.get('DATABASE_PASSWORD')
+	database_host = app.config.get('DATABASE_HOST')
+	return "postgres://{}:{}@{}/{}".format(database_user, database_password, database_host, database_name)
+	# return "postgresql:///fsdn-farm-shop"
 
 def setup_db(app):
 	app.config["SQLALCHEMY_DATABASE_URI"] = get_database_path(app)
