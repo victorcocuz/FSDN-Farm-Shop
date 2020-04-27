488b3635b5ea5c3ab4c9bace81f8adede940b23a
Vitor Cocuz <victor.cocuz@yahoo.com>
2020-04-27 13:32:38 +0100

    again tweaked the config. still can't get config to work despide following all the examples online...

--- config.py
+++ config.py
@@ -10,7 +10,8 @@ class Config(object):
 	AUTH0_AUDIENCE = 'farm_shop_api'
 	AUTH0_RESPONSE_TYPE = 'token'
 	AUTH0_CLIENT_ID = 'NCvcf0H0XLMpzg5xWGh3jJGcqve0or5c'
-	AUTH0_REDIRECT = 'http://127.0.0.1:5000/'
+	# AUTH0_REDIRECT = 'http://127.0.0.1:5000/'
+	AUTH0_REDIRECT = 'https://fsdn-farm-shop.herokuapp.com/'
 	REQUEST_URI = "https://{}/authorize?audience={}&response_type={}&client_id={}&redirect_uri={}".format(AUTH0_DOMAIN, AUTH0_AUDIENCE, AUTH0_RESPONSE_TYPE, AUTH0_CLIENT_ID, AUTH0_REDIRECT)
 
 	JWT_SECRET_KEY = 'this-is-my-jwt-secret-key'
